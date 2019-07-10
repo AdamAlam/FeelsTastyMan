30c634df98cbf86ad0e4ebc58b2a501656a563e0
AdamAlam <46464084+AdamAlam@users.noreply.github.com>
2019-07-10 17:38:15 -0500

    Added database

--- feelstasty.py
+++ feelstasty.py
@@ -1,13 +1,34 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect, flash
+from flask_sqlalchemy import SQLAlchemy
+from wtforms import Form, BooleanField, StringField, PasswordField, validators
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = "918828023105ed02141c31eb71b06497"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/feelstasty'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column('id', db.Integer, primary_key=True)
+    
+
+
+class RegistrationForm(Form):
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email Address', [validators.Length(min=6, max=35)])
+    password = PasswordField('New Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Password must match')
+    ])
+    confirm = PasswordField('Repeat Password')
 
 
 @app.route("/")
 def home():
     return render_template("home.html", title="FeelsHomeMan")
+
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
@@ -16,4 +37,9 @@ def login():
 
 @app.route('/register', methods=["POST", "GET"])
 def register():
-    return render_template("register.html", title='FeelsRegisterMan')
+    form = RegistrationForm(request.form)
+    if request.method == "Post" and form.validate():
+        user = User(form.username.data, form.email.datam, form.password.data)
+        db_session.add(user)
+        return redirect(url_for('login'))
+    return render_template("register.html", title='FeelsRegisterMan', form=form)
